4d245aea8d4e58837b5fa9f4882a64b06861cf1f
Nelio Batista <nelio.batistadonasci@ndus.edu>
2021-10-14 10:57:45 -0300

    Initial Commits

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,11 +8,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.ConnectionSpec;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         OkHttpClient client = new OkHttpClient();
 
         String url = "https://reqres.in/api/users?pages=1";
-
+        /*String url = "http://181.222.254.54/users";*/
         Request request = new Request.Builder().url(url).build();
 
         client.newCall(request).enqueue(new Callback() {
@@ -54,9 +59,13 @@ public class MainActivity extends AppCompatActivity {
     public void requesterOn (View view) {
         mTextViewResult1 = findViewById(R.id.textView1);
 
-        OkHttpClient client2 = new OkHttpClient();
+        OkHttpClient client2 = new OkHttpClient.Builder()
+                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
+                .build();
 
-        String url2 = "https://reqres.in/api/users?page=3";
+        /*OkHttpClient client2 = new OkHttpClient();*/
+
+        String url2 = "http://181.222.254.54/users";
 
         Request request = new Request.Builder().url(url2).build();
 
@@ -72,19 +81,71 @@ public class MainActivity extends AppCompatActivity {
                     assert response.body() != null;
                     String myResponse2 = response.body().string();
 
-                    MainActivity.this.runOnUiThread(() -> mTextViewResult1.setText(myResponse2));
+                   MainActivity.this.runOnUiThread(() -> mTextViewResult1.setText(myResponse2));
                 }
             }
         });
     }
 
-    public String requestPost(View view) throws IOException {
-        PostExample example = new PostExample();
-        String json = example.bowlingJson("Jesse", "Jake");
-        String response = example.post("http://www.roundsapp.com/post", json);
-        System.out.println(response);
+    public void requestPost (View view) {
+        /*mTextViewResult1 = findViewById(R.id.textView1);*/
 
+        OkHttpClient client3 = new OkHttpClient.Builder()
+                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
+                .build();
+
+        /*OkHttpClient client2 = new OkHttpClient();*/
+
+        String url2 = "http://181.222.254.54/users";
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("Tinelio", "Aqui")
+                .build();
+        Request request = new Request.Builder()
+                /*.url("http://181.222.254.54/users")*/
+                .url("http://192.168.0.243/users")
+                .post(formBody)
+                .build();
+
+        client3.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    assert response.body() != null;
+                    String myResponse2 = response.body().string();
+
+                    /*MainActivity.this.runOnUiThread(() -> mTextViewResult1.setText(myResponse2));*/
+                }
+            }
+        });
     }
 
+    /*public void requestPost(View view) throws IOException {
+
+        OkHttpClient client3 = new OkHttpClient.Builder()
+                .connectionSpecs(Arrays.asList(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
+                .build();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("hello", "world")
+                .build();
+        Request request = new Request.Builder()
+                .url("http://181.222.254.54/users")
+                .post(formBody)
+                .build();
+
+        try {
+            Response response = client3.newCall(request).execute();
+
+            // Do something with the response.
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
 
 }
--- dummy-api/api.py
+++ dummy-api/api.py
@@ -1,14 +1,43 @@
 from flask import Flask, request
+from flask import abort, current_app as app
 
 import json
 
-
+global example
+example = {
+    "glossary": {
+        "title": "example glossary",
+		"GlossDiv": {
+            "title": "S",
+			"GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+					"SortAs": "SGML",
+					"GlossTerm": "Standard Generalized Markup Language",
+					"Acronym": "SGML",
+					"Abbrev": "ISO 8879:1986",
+					"GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+						"GlossSeeAlso": ["GML", "XML"]
+                    },
+					"GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
 app = Flask(__name__)
 
+# ip_white_list = ['187.181.64.79']
+# @app.before_request
+# def block_method():
+#     ip = request.environ.get('REMOTER_ADDR')
+#     if ip not in ip_white_list:
+#         abort(403)
 @app.route('/users', methods=['GET','POST'])
 def home():
     print(request.form)
-    return 'hello world!'
+    return example
 
 if __name__ == "__main__":
     app.run(debug=True,host='192.168.0.243',port=80)
